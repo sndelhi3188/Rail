3502e1b3473943d6cdeb08beb92263a8c804a5ff
Sudheer Nayak <sndelhi3188@gmail.com>
2013-08-07 17:46:44 +0530

    CRUD created for post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,15 +4,48 @@ def new
 end
 
 def create
-	#render text: params[:post].inspect
-	@post = POST.new(post_params)
-	@post.save
-	redirect_to @post
+  @post = Post.new(post_params)
+ 
+  if @post.save
+     redirect_to @post
+  else
+     render 'new'
+  end 
 end
 
-#private 
-#def
-#	params.require(:post).permit(:Title, :Comment)
-#end
+def index
+  @post = Post.all
+end
+
+def edit
+  @post = Post.find(params[:id])
+end
+
+def destroy
+  @post = Post.find(params[:id])
+  @post.destroy
+  redirect_to post_path
+#  render 'index'
+end
+
+def update
+  @post = Post.find(params[:id])
+	
+	if @post.update(params[:post].permit(:Title, :Comment))
+	   redirect_to @post
+	else
+	   render 'edit'
+	end
+end
+
+private
+  def post_params
+    params.require(:post).permit(:Title, :Comment)
+  end
+
+def show
+#  @post = Post.find(params[:id])
+render text: params[:post].inspect
+end
 
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,3 @@
 class Post < ActiveRecord::Base
+validates :Title, presence: true, length: {minimum: 5 }
 end
